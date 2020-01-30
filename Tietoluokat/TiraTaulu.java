288612d9b358dd8291877daac5cf673b5ab411ee
Helen Konttinen <helen.konttinen@tuni.fi>
2020-01-30 15:49:53 +0200

    TiraTaulu -luokkaan tehty muutokset.

--- Tietoluokat/TiraTaulu.java
+++ Tietoluokat/TiraTaulu.java
@@ -7,40 +7,36 @@ package Harjoitustyo.Tietoluokat;
       public void lisaaTaulukko (int[] taulukko, int sourcefile){
          
          for (int i = 0; i < taulukko.length; i++) {
-            Node valiaikainen = new Node(taulukko[i], sourcefile, i);
-            lisaaNode(valiaikainen);
+            int apu = taulukko[i];
+            Lokero input = new Lokero(apu, new Node(apu, sourcefile, i+1));
+         
+            if (koko == 0) {
+               paa = hanta = valiaikainen;
+            }
+            else {
+               for (Lokero valiaikainen = paa; valiaikainen.seuraava() != null; valiaikainen = valiaikainen.seuraava()) {
+                  //jos kyseinen indeksi löytyy taulusta...
+                  if (valiaikainen.indeksi() == input.indeksi()) {
+                     //...kasvatetaan indeksiä kunnes löyetään vapaa lokero
+                     input.indeksi(input.ineksi()+1);
+                  }
+               }
+               //sijoitetaan alkio
+               lisaaNode(input);
+            }
+            //kasvatetaan taulun kokoa
+            koko++;
          }
       }
       
       /*lisäysoperaation toinen osa */
-      public void lisaaNode (Node input) {
-         int apu = Integer.valueOf(input.data().toString());
-         Lokero valiaikainen = new Lokero(apu, input);
-         
-         if (koko == 0) {
-            paa = hanta = valiaikainen;
-         } 
-         else {
-            //tarkistetaan onko jo olemassa lokeroa kyseisellä indeksillä
-            while (loyda(valiaikainen.indeksi())) {
-               //kasvatetaan moduloa ja indeksiä yhdellä kunnes löydetään vapaa lokero
-               valiaikainen.mod(valiaikainen.mod() +1);
-               valiaikainen.indeksi(valiaikainen.indeksi() +1);
-            }
-            // sijoitetaan alkio lopulliselle paikalle
-            sijoita(valiaikainen);
-         }
-         koko++;
-      }
-      
-      /*lisäysoperaation sijoittava osa */
-      public void sijoita (Lokero input) {
-         
+      public void lisaaNode (Lokero input) {
+
          //jos sijoitettava alkio on pienin
+         //otetaan talteen pää -lokero ja asetetaan uudeksi
+         //pää lokeroksi sijoitettava alkio. Lopuksi liitettän
+         //entinen pää -lokero nykyiseen pää -lokeroon edellinen -viittauksella
          if (input.indeksi() < paa.indeksi()) {
-            //otetaan talteen pää -lokero ja asetetaan uudeksi
-            //pää lokeroksi sijoitettava alkio. Lopuksi liitettän
-            //entinen pää -lokero nykyiseen pää -lokeroon edellinen -viittauksella
             Lokero valiaikainen = paa;
             paa = input;
             input.seuraava(valiaikainen);
@@ -48,33 +44,34 @@ package Harjoitustyo.Tietoluokat;
             
          } 
          //kun sijoitettava alkio on suurin
+         //otetaan talteen häntä -lokero väliaikaiseen muuttujaan.
+         //asetetaan sijoitettava alkio uudeksi häntä -lokeroksi.
+         //Lopuksi liitetään entinen häntä -lokero uuteen häntä -lokeroon.
          else if (input.indeksi() > hanta.indeksi()) { 
-            //otetaan talteen häntä -lokero väliaikaiseen muuttujaan.
-            //asetetaan sijoitettava alkio uudeksi häntä -lokeroksi.
-            //Lopuksi liitetään entinen häntä -lokero uuteen häntä -lokeroon.
             Lokero valiaikainen = hanta;
             hanta = input;
             input.edellinen(valiaikainen);
             valiaikainen.seuraava(hanta);
             
          } 
-         // muut tapaukset
+         // muut tapaukset:
+         //otetaan talteen pää -lokero ja siitä seuraava lokero.
+         //käydään lokeroiden indeksit läpi
+         //jos sijoitettavan alkion indeksi on suurempi kuin pään -indeksi ja pienempi tai yhtä
+         //suuri kuin päästä seuraavan alkion indeksi...
+         //pää -lokeron seuraavaksi lokeroksi asetetaan sijoitettava alkio
+         //päästä seuraavan edelliseksi lokeroksi asetetaan sijoitettava alkio
+         //sijoitettavan alkion edelliseksi lokeroksi asetetaan entinen pää-lokero.
+         //sijoitettavan alkion seuraavaksi lokeroksi asetetaan entisestä pää-lokerosta seuraava lokero.
          else { 
-            //otetaan talteen pää -lokero ja siitä seuraava lokero.
             Lokero valiaikainen = paa;
             Lokero tempSeuraava = paa.seuraava();
-            //käydään lokeroiden indeksit läpi
+            
             for (long i = 0; i < koko - 1; i++) {
-               //jos sijoitettavan alkion indeksi on suurempi kuin pään -indeksi ja pienempi tai yhtä
-               //suuri kuin päästä seuraavan alkion indeksi...
-               if (input.indeksi() > valiaikainen.indeksi() && input.indeksi() <= tempSeuraava.indeksi()) {
-                  //pää -lokeron seuraavaksi lokeroksi asetetaan sijoitettava alkio
+               if (input.indeksi() > valiaikainen.indeksi() && input.indeksi() <= tempSeuraava.indeksi()) { 
                   valiaikainen.seuraava(input);
-                  //päästä seuraavan edelliseksi lokeroksi asetetaan sijoitettava alkio
                   tempSeuraava.edellinen(input);
-                  //sijoitettavan alkion edelliseksi lokeroksi asetetaan entinen pää-lokero.
                   input.edellinen(valiaikainen);
-                  //sijoitettavan alkion seuraavaksi lokeroksi asetetaan entisestä pää-lokerosta seuraava lokero.
                   input.seuraava(tempSeuraava);
                } 
                else {
@@ -84,17 +81,103 @@ package Harjoitustyo.Tietoluokat;
                }
             }
          }
+
+         /*else {
+            //tarkistetaan onko jo olemassa lokeroa kyseisellä indeksillä
+            while (loyda(valiaikainen.indeksi())) {
+               //kasvatetaan moduloa ja indeksiä yhdellä kunnes löydetään vapaa lokero
+               valiaikainen.mod(valiaikainen.mod() +1);
+               valiaikainen.indeksi(valiaikainen.indeksi() +1);
+            }
+            // sijoitetaan alkio lopulliselle paikalle
+            sijoita(valiaikainen);
+         }
+         koko++;*/
       }
       
-      /* apuoperaatio joka tarkistaa onko kyseistä indeksiä taulussa */
-      public boolean loyda (int k) {
-         for (Lokero valiaikainen = paa; valiaikainen.seuraava() != null; valiaikainen = valiaikainen.seuraava()) {
-            //jos kyseinen indeksi löytyy taulusta palautetaan true
-            if (valiaikainen.indeksi() == k) {
-               return true;
+      /*joukko-operaatioille lisäysoperaatio*/
+      public void lisaaLokero (Lokero item) {
+         Lokero tmp = new Lokero(item.node().data(), item.node());
+         boolean onSijoitettu = false;
+
+         if (koko == 0) {
+            paa = hanta = item;
+            koko++;
+         }
+         else {
+            for (Lokero p = paa; p != null; p = p.seuraava()) {
+               if (tmp.node().data() == p.node().data()) {
+                  int apu = p.node().amount();
+                  p.node().amount(apu + 1);
+                  onSijoitettu = true;
+                  p = hanta;
+               }
+            }
+            if (!onSijoitettu) {
+               lisaaNode(item);
+               koko++;
             }
          }
-         return false;
+      }
+
+      /*listan pienimmän alkion poistava operaatio*/
+      public Lokero poistaPaa () {
+         if (koko != 0) {
+            Lokero tmp = new Lokero(paa.node().data(), paa.node());
+
+            if (koko > 1) {
+               paa = paa.seuraava();
+               head.edellinen(null);
+            }
+            else {
+               paa = hanta = null;
+            }
+            koko--;
+            return tmp;
+         }
+         else {
+            return null;
+         }
+      }
+
+      /*alkion poistaminen linkitetystä listasta*/
+      public boolean poista (int poistettava) {
+         boolean onnistui = false;
+         if (koko != 0) {
+            //kun poistettava alkio on pienin
+            if ((koko == 1) || paa.node().data() == poistettava) {
+               poistaPaa();
+               onnistui = true;
+            }
+            //kun poistettava alkio on suurin
+            else if (hanta.node().data() == poistettava) {
+               hanta = hanta.edellinen();
+               hanta.seuraava(null);
+               koko--;
+               onnistui = true;
+            }
+            //muut tapaukset
+            else {
+               for (Lokero tmp = paa; tmp != null; tmp = tmp.seuraava()) {
+                  if (tmp.node().data() == poistettava) {
+                     tmp.edellinen().seuraava(tmp.seuraava());
+                     tmp.seuraava().edellinen(tmp.edellinen());
+                     koko--;
+                     onnistui = true;
+                     tmp = hanta;
+                  }
+               }
+            }
+         }
+         return onnistui;
+      }
+
+      public void print() {
+         for (Lokero tmp = paa; tmp != null; tmp = tmp.seuraava()) {
+            System.out.print("Indeksi: " + tmp.indeksi() + ", data: " + tmp.node().data() + ", ");
+            System.out.println("Rivi: " + tmp.node().rivi() + ", määrä: " + tmp.node.amount());
+         }
+         System.out.println("Listan koko: " + koko);
       }
       
 
@@ -105,13 +188,29 @@ package Harjoitustyo.Tietoluokat;
       }
 
       /* Setterit */
-      public void asetaKoko (int k) {
+      public void koko (int k) {
          koko = k;
+      }
+
+      public void paa (Lokero p) {
+         paa = p;
+      }
+
+      public void hanta (Lokero h) {
+         hanta = h;
       }
       
       /* Getterit */
       public int koko () {
          return koko;
+      }
+
+      public Lokero paa () {
+         return paa;
+      }
+
+      public Lokero hanta () {
+         return hanta;
       }
       
       private int koko;
