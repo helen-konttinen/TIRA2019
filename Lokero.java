f241e9a21b7f202126349c60223a2d04958b32f4
Helen Konttinen <helen.konttinen@tuni.fi>
2020-01-31 21:36:24 +0200

    Joukko -operaatioiden lisays ja luokkien viimeistely.

--- Lokero.java
+++ Lokero.java
@@ -1,5 +1,3 @@
-package Harjoitustyo.Tietoluokat;
-
 public class Lokero {
 
    public Lokero (int i, Node n) {
--- Tira2019.java
+++ Tira2019.java
@@ -1,21 +1,27 @@
-package Harjoitustyo;
+/*
+ * Tietorakenteet 2019 -kurssin harjoitustyö.
+ * Harjoitustyössä suoritetaan joukko -operaatioita tiedostoille
+ * käyttäen apuna hajautustaulua.
+ * 
+ * Helen Konttinen
+ * helen.konttinen@tuni.fi
+ * */
 
+import java.lang.Integer;
+import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.lang.Integer;
-import java.util.*;
-import Harjoitustyo.Tietoluokat.*;
 
 public class Tira2019 {
-    /* the main loop which runs the coursework */
+    /* pääsilmukka joka ajaa harjoitustyötä*/
     public void run() {
-        System.out.println("Coursework handles numbers as Integers. The hashfunction used is (+1),");
-        System.out.println("which means that overflow is possible by trying to enter");
-        System.out.println("Integer.MAX_VALUE twice in a row or a group of values that would");
-        System.out.println("end up exceeding Integer.MAX_VALUE.\n");
+        System.out.println("Harjoitustyö käsittelee numeroita kokonaislukuina. Hajautusfunktiona käytetään (+1),");
+        System.out.println("mikä tarkoittaa, että ylivuoto on mahdollista jos yrittää antaa arvoksi");
+        System.out.println("Integer.MAX_VALUE kahdesti peräkkäin tai jos antaa arvoksi ryhmän arvoja, jotka");
+        System.out.println("ylittäisivät Integer.MAX_VALUE.\n");
         
         TiraTaulu htA = new TiraTaulu();
         int[] setA = lueInput("setA.txt");
@@ -27,8 +33,8 @@ public class Tira2019 {
         Scanner sc = new Scanner(System.in);
 
         while (run) {
-            System.out.println("\nUse commands \"or\", \"and\" and \"xor\" to perform actions.");
-            System.out.println("You can exit the program by using \"exit\" -command.");
+            System.out.println("\nKäytä komentoja \"or\", \"and\" ja \"xor\" toimintojen suorittamiseksi.");
+            System.out.println("Voit poistua ohjelmasta \"exit\" -komennolla.");
             String s = sc.nextLine();
             String command = s.substring(0, 1);
             
@@ -37,7 +43,7 @@ public class Tira2019 {
                 run = false;
             } 
             else {
-                // import the data from txt-files incase user doesnt wish to exit.
+                //importoi data txt-tiedostoista jos käyttäjä ei halua poistua ohjelmasta
                 htA.lisaaTaulukko(setA, 1);
                 htB.lisaaTaulukko(setB, 2);
                 
@@ -51,14 +57,16 @@ public class Tira2019 {
                     exclude(htA, htB);
                 } 
                 else {
-                    System.out.println("incorrect input");
+                    System.out.println("Väärä syöte");
                 }
             }
         }
-        System.out.println("\nCoursework closed.");
+        sc.close();
+        System.out.println("\nHarjoitustyö suljettu.");
     }
 
-    public void union(TiraTaulu htA, TiraTaulu htB) {  // and-operation
+    // and-toiminto
+    public void union(TiraTaulu htA, TiraTaulu htB) {
         TiraTaulu A = htA;
         TiraTaulu B = htB; 
         TiraTaulu C = new TiraTaulu();
@@ -74,14 +82,15 @@ public class Tira2019 {
         while (A.koko() > 0) {
            C.lisaaLokero(A.poistaPaa());
         }
-        while (B.size() > 0) {
+        while (B.koko() > 0) {
            C.lisaaLokero(B.poistaPaa());
         }
         
         tulostaminen("or", C);
      }
   
-     public void intersection(TiraTaulu htA, TiraTaulu htB){ // or-operation
+     // or-toiminto
+     public void intersection(TiraTaulu htA, TiraTaulu htB){
         TiraTaulu A = htA;
         TiraTaulu B = htB;
         TiraTaulu C = new TiraTaulu();
@@ -105,7 +114,8 @@ public class Tira2019 {
         tulostaminen("and", C);
      }
   
-     public void exclude(TiraTaulu htA, TiraTaulu htB){  // xor-operation
+     // xor-toiminto
+     public void exclude(TiraTaulu htA, TiraTaulu htB){
         TiraTaulu A = htA;
         TiraTaulu B = htB;
         TiraTaulu C = new TiraTaulu();
@@ -127,12 +137,78 @@ public class Tira2019 {
         while (A.koko() > 0) {
            C.lisaaLokero(A.poistaPaa());
         }
-        while(B.size() > 0){
+        while(B.koko() > 0){
            C.lisaaLokero(B.poistaPaa());
         }
         
         tulostaminen("xor", C);
-     }
+    }
+
+    /*toiminto, listan ja tulostuksen viimeistelyyn */
+    public void tulostaminen (String operation, TiraTaulu C) {
+        boolean more = true;
+        while (more) {
+            System.out.print("\nPaina \"1\" jos haluat poistaa numeron ");
+            System.out.println(operation + "-hajautustaulusta,");
+            System.out.println("      \"0\" jos haluat jatkaa ilman numeron poistoa");
+            Scanner ab = new Scanner(System.in);
+            int input = ab.nextInt();
+            
+            if (input == 1) {
+                delete(C);
+            } 
+            else if (input == 0) {
+                more = false;
+            } 
+            else {
+                System.out.println("Väärä syöte");
+            }
+        }
+        
+        // muodostetaan tulostettava taulukko toivottuun muotoon
+        String[] result = new String[C.koko()];
+        int secondaryValue = Integer.MAX_VALUE;
+        Lokero tmp = C.paa();
+        
+        for (int i = 0; i < result.length; i++) {
+            // täydennetään taulukon toinen kolumni toivotulla arvolla
+            if (operation == "or") {
+                secondaryValue = tmp.node().amount();
+            } 
+            else if (operation == "and") {
+                secondaryValue = tmp.node().rivi();
+            } 
+            else if (operation == "xor") {
+                secondaryValue = tmp.node().source();
+            }
+            
+            result[i] = tmp.node().data() + "   " + secondaryValue;
+            tmp = tmp.seuraava();
+        }
+        writeOutput(result, operation + ".txt");
+        System.out.println("Toiminto \"" + operation + "\" onnistui, tuloksena joukko, joka ");
+        System.out.println("sisältää " + result.length + " alkiota.");
+    }
+
+    /* operation which performs the deletion of values */
+    public void delete (TiraTaulu table) {
+        if (table.koko() > 0) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("\nAnna arvo, jonka haluat poistaa: ");
+            int input = sc.nextInt();
+            
+            if (table.poista(input)){
+                System.out.println("Arvon poisto onnistui.");
+            } 
+            else {
+                System.out.println("Arvoa ei löydetty.");
+            }
+            sc.close();
+        } 
+        else {
+            System.out.println("Taulu on tyhjä, ei voida poistaa arvoa.");
+        }
+    }
 
 	public int[] lueInput(String luettavaTiedosto) {
 		String line;
@@ -148,16 +224,14 @@ public class Tira2019 {
 
             //luodaan väliaikainen taulukko tiedoston sisältö varten
             int[] sisalto = new int[koko];
-
             BufferedReader br2 = new BufferedReader(new FileReader(luettavaTiedosto));
             
             //luetaan tiedosto uudestaan läpi, tällä kertaa tallettaen tiedoston sisältö
             //sisalto -taulukkoon
             for (int i = 0; i < koko; i++) {
-                line = br2.readLine();
+                line = br2.readLine().trim();
                 String[] tmp = line.split("\n");
                 sisalto[i] = Integer.parseInt(tmp[0]);
-                System.out.println(sisalto[i]);
             }
             br2.close();
             //viitataan valiaikais taulukkoon, sisalto
@@ -169,16 +243,24 @@ public class Tira2019 {
         catch(Exception e) {
             System.out.println(e);
         }
+        System.out.println("Tiedosto \"" + luettavaTiedosto + "\" luettu onnistuneesti.");
         return palautettavaTaulukko;
-	}
-    public void run() {
-        //luodaan uusi taulu ja luodaan siihen taulukkoja
-        TiraTaulu hajautustaulu = new TiraTaulu();   
-        int[] setA = lueInput("setA.txt");
-        hajautustaulu.lisaaTaulukko(setA, 1);
-        int[] setB = lueInput("setB.txt");
-        hajautustaulu.lisaaTaulukko(setB, 2);
-        
+    }
+    
+    /* operation which updates the result files */
+   public void writeOutput(String[] output, String operation){
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(operation));	
+            for (int i = 0; i < output.length; i++){
+                bw.write(output[i]);
+                bw.newLine();
+            }
+            bw.close();
+        } 
+        catch (IOException e) {
+            System.err.format("IOException: %s%n", e);
+        }
+        System.out.println("\nKirjoitetaan tiedostoa \"" + operation + "\".");
     }
    
     public static void main (String[] args) { 
@@ -186,14 +268,5 @@ public class Tira2019 {
         ht.run();
 
     }
-
-    /*public void and (TiraTaulu A, TiraTaulu B) {
-        for (int i = 0; i < A.koko(); i++) {
-            A.
-            for (int j = 0; i < B.koko(); j++) {
-
-            }
-        }   
-    } */
 
 }
